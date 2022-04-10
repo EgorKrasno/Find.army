9c287229fe922e970bc0e3ff54dbf0f61b3818d5
Egor Kras <egor.krasnonosenkikh.mil@swf.army.mil>
2022-04-09 19:08:45 -0500

    fix icon colors v5

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,83 @@ import {
 import {arrayMove, rectSortingStrategy, SortableContext, sortableKeyboardCoordinates,} from '@dnd-kit/sortable';
 import Wrapper from "../components/Wrapper";
 import LinkBlock from "../components/LinkBlock";
-import {exploreData} from "../data/exporeData";
+import {AiOutlineMail} from "react-icons/ai";
+import {MdAttachMoney} from "react-icons/md";
+import {GiClothes, GiComputing, GiHealthNormal} from "react-icons/gi";
+import {BsFillBriefcaseFill} from "react-icons/bs";
+
+const exploreData = [
+  {
+    id: '1',
+    title: 'Army Email',
+    icon: <AiOutlineMail className='dark:fill-yellow-300 fill-blue-400' size={128}/>,
+    cac: true,
+    href: 'https://webmail.apps.mil/mail',
+    description: 'Test 5 description',
+  },
+  {
+    id: '2',
+    title: 'MyPay',
+    icon: <MdAttachMoney className='dark:fill-yellow-300 fill-blue-400' size={128}/>,
+    cac: false,
+    href: 'https://mypay.dfas.mil/#/',
+    description: 'Test 6 description',
+    tags: ['pay', 'les', 'money'],
+  },
+  {
+    id: '3',
+    title: 'MedPros',
+    icon: <GiHealthNormal className='dark:fill-yellow-300 fill-blue-400' size={128}/>,
+    cac: true,
+    href: 'https://medpros.mods.army.mil/portal',
+    description: 'Test 6 description',
+    tags: ['medical'],
+  },
+  {
+    id: '4',
+    title: 'My Clothing Record',
+    icon: <GiClothes className='dark:fill-yellow-300 fill-blue-400' size={128}/>,
+    cac: true,
+    href: 'https://ism.army.mil/ism/SelfServiceServlet?nav.nav_id=ssMyClothing',
+    description: 'Test 6 description',
+    tags: ['clothing'],
+  },
+  {
+    id: '5',
+    title: 'TRICARE',
+    icon: <GiHealthNormal className='dark:fill-yellow-300 fill-blue-400' size={128}/>,
+    cac: false,
+    href: 'https://tricare.mil/',
+    description: 'Test 6 description',
+    tags: ['medical', 'health', 'dental', 'vision', 'insurance'],
+  },
+  {
+    id: '6',
+    title: 'Army Career Tracker (ACT)',
+    icon: <BsFillBriefcaseFill className='dark:fill-yellow-300 fill-blue-400' size={128}/>,
+    cac: true,
+    href: 'https://actnow.army.mil/',
+    description: 'Test 6 description',
+    tags: ['sponsorship', 'career'],
+  },
+  {
+    id: '7',
+    title: 'AIM 2.0',
+    icon: <BsFillBriefcaseFill className='dark:fill-yellow-300 fill-blue-400' size={128}/>,
+    cac: true,
+    href: 'https://aim.hrc.army.mil/',
+    description: 'Test 6 description',
+    tags: ['officer', 'orb'],
+  },
+  {
+    id: '8',
+    title: 'Cyber Awareness Challenge',
+    icon: <GiComputing className='dark:fill-yellow-300 fill-blue-400' size={128}/>,
+    cac: true,
+    href: 'https://cs.signal.army.mil/',
+    description: 'Test 6 description',
+  },
+];
 
 
 const fuse = new Fuse(exploreData, {
