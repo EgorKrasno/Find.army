4187090962f1c1e1d3f12d608f8a572a621e7a0b
Egor Kras <egor.krasnonosenkikh.mil@swf.army.mil>
2022-04-13 14:04:58 -0500

    vercel push

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -53,7 +53,7 @@ const Nav = () => {
               </Link>
 
             </div>
-            <div className="flex items-center space-x-4 sm:space-x-6">
+            <div className="flex items-center space-x-4 sm:space-x-6 ">
               <div
                 onClick={(e) => {
                   setIsFeedbackModalOpen(true);
