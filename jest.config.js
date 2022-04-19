190581baa5d66f08d8d51aab64656495bb614c8a
Egor Kras <egor.krasnonosenkikh.mil@swf.army.mil>
2022-04-18 20:15:13 -0500

    fix tests

--- jest.config.js
+++ jest.config.js
@@ -10,14 +10,4 @@ const customJestConfig = {
     testEnvironment: 'jest-environment-jsdom',
 }
 
-const ignoredErrors = [
-    /act(...) is not supported in production builds of React, and might not behave as expected./,
-]
-const consoleError = global.console.error
-global.console.error = (...args) => {
-    if (ignoredErrors.some((el) => el.test(args[0]))) {
-        return consoleError(...args)
-    }
-}
-
 module.exports = createJestConfig(customJestConfig)
