f5e366c6725a9ac2492b640062f9d150d252d176
uplusion23 <uplusion23@gmail.com>
2024-06-26 11:18:53 -0500

    Add footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,10 @@
 import { CardsContextProvider } from '@/components/hooks/use-blocks';
 import { Nav } from '@/components/nav/nav';
 import { ThemeProvider } from '@/components/theme-provider';
+import { Button } from '@/components/ui/button';
 import { TooltipProvider } from '@/components/ui/tooltip';
 import { Metadata } from 'next';
+import Link from 'next/link';
 import './globals.css';
 
 export const metadata: Metadata = {
@@ -28,6 +30,22 @@ export default function RootLayout({
                 vaul-drawer-wrapper="">
                 {children}
               </div>
+
+              <footer className="mt-4 rounded-t border-t bg-card">
+                <div className="mx-auto w-full max-w-screen-lg px-4 py-4 sm:py-6 md:flex md:items-center md:justify-between">
+                  <span className="text-sm text-muted-foreground sm:text-center">
+                    © {new Date().getFullYear()}
+                    <Button
+                      variant="link"
+                      size="sm"
+                      className="px-1 py-0 font-mono text-muted-foreground"
+                      asChild>
+                      <Link href="/">Find.Army</Link>
+                    </Button>
+                  </span>
+                  {/* <ul className="mt-3 flex flex-wrap items-center text-sm font-medium text-muted-foreground sm:mt-0"></ul> */}
+                </div>
+              </footer>
             </CardsContextProvider>
           </TooltipProvider>
         </ThemeProvider>
